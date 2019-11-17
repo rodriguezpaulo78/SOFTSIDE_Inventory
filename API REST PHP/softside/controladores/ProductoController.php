4ca3a366c630e6d15657e20b9ed9bf8c7720836f
Paulo Rodriguez <rodriguezpaulo78@gmail.com>
2019-11-17 11:54:33 -0500

    Servicios REST para Gestión Productov1.0

---
+++ API REST PHP/softside/controladores/ProductoController.php
@@ -0,0 +1,143 @@
+<?php
+
+include_once '../conexion/db-connect.php';
+
+class ProductoController{
+	private $db;
+	private $db_table = "producto";
+	public function __construct(){
+		$this->db = new DbConnect();
+
+	}
+	
+	public function getProductoCod(){
+
+		$query = "SELECT LPAD((SELECT COUNT(*) + 1 FROM ".$this->db_table."), 5, '0') AS nextCod";
+		$result = mysqli_query($this->db->getDb(),$query);
+		$cod = "00000";
+
+		if(mysqli_num_rows($result) > 0){
+ 			while($row = mysqli_fetch_assoc($result)){
+				$cod = $row["nextCod"];
+			}
+ 			mysqli_close($this->db->getDb());
+ 		}
+
+		$json = array();
+		$json['codigo'] = $cod;
+
+		return $json;
+	}
+
+	public function agregarProducto($datos){	
+		$json = array();
+
+		$query = "INSERT INTO ".$this->db_table."(prod_id, prod_nombre, prod_descripcion, unidad_id, prod_fec_venc, proveedor_id) VALUES ('$datos[0]', '$datos[1]', '$datos[2]', '$datos[3]', '$datos[4]', '$datos[5]')";
+
+		$inserted = mysqli_query($this->db->getDb(), $query);
+
+		if($inserted == 1){
+			$json['message'] = "SUCCESS";
+		}else{
+			$json['message'] = "FAILED";
+		}
+		mysqli_close($this->db->getDb());
+			
+		
+		return $json;
+	}
+
+	public function modificarProducto($datos){	
+		$json = array();
+
+		$query = "UPDATE ".$this->db_table." SET prod_nombre='$datos[1]', prod_descripcion='$datos[2]', unidad_id='$datos[3]', prod_fec_venc='$datos[4]', proveedor_id='$datos[5]' WHERE prod_id='$datos[0]'";
+
+		$updated = mysqli_query($this->db->getDb(), $query);
+
+		if($updated == 1){
+			$json['message'] = "SUCCESS";
+		}else{
+			$json['message'] = "FAILED";
+		}
+		mysqli_close($this->db->getDb());
+		
+		return $json;
+	}
+
+	public function listarProductos(){
+		$query = "SELECT prod_id, prod_nombre, prod_descripcion, unidad_id, prod_fec_venc, proveedor_id, user_est_reg FROM ".$this->db_table;
+		$result = mysqli_query($this->db->getDb(),$query);
+
+		$json = array();
+		if(mysqli_num_rows($result) > 0){
+			$i = 0;
+ 			while($row = mysqli_fetch_assoc($result)){
+				$json[$i]=$row;
+				$i++;
+			 }
+ 		}
+
+ 		mysqli_close($this->db->getDb());
+		return $json;
+	}
+
+	public function getProductoByCod($codigo){
+		$query = "SELECT prod_id, prod_nombre, prod_descripcion, unidad_id, prod_fec_venc, proveedor_id, user_est_reg FROM ".$this->db_table." WHERE prod_id='".$codigo."'";
+		$result = mysqli_query($this->db->getDb(),$query);
+
+		$json = array();
+		if(mysqli_num_rows($result) > 0){
+			$i = 0;
+ 			while($row = mysqli_fetch_assoc($result)){
+				$json[$i]=$row;
+				$i++;
+			 }
+ 		}
+
+ 		mysqli_close($this->db->getDb());
+		return $json;
+	}
+
+	public function eliminarProducto($codigo){	
+		$json = array();
+
+		$query = "UPDATE ".$this->db_table." SET prod_est_reg='I' WHERE prod_id='".$codigo."'";
+
+		$deleted = mysqli_query($this->db->getDb(), $query);
+
+		if($deleted == 1){
+			$json['message'] = "SUCCESS";
+		}else{
+			$json['message'] = "FAILED";
+		}
+		mysqli_close($this->db->getDb());
+		
+		return $json;
+	}
+
+	public function buscarProducto($dato, $filtro){
+		$query = "";
+
+		if ($filtro == "Nombre")
+			$query = "SELECT prod_id FROM ".$this->db_table." WHERE prod_nombre='".$dato."'";
+		elseif ($filtro == "Unidad")
+			$query = "SELECT prod_id FROM ".$this->db_table." WHERE unidad_id='".$dato."'";
+		elseif ($filtro == "Proveedor")
+			$query = "SELECT prod_id FROM ".$this->db_table." WHERE proveedor_id='".$dato."'";
+
+		$result = mysqli_query($this->db->getDb(),$query);
+
+		$json = array();
+		if(mysqli_num_rows($result) > 0){
+			$i = 0;
+ 			while($row = mysqli_fetch_assoc($result)){
+				$json[$i]=$row;
+				$i++;
+			 }
+ 		}
+
+ 		mysqli_close($this->db->getDb());
+		return $json;
+	}
+}
+?>
---
+++ API REST PHP/softside/modelos/ProductoModel.php
@@ -0,0 +1,82 @@
+<?php
+	//Para cargar todos lo archivos dentro de librerias/*
+	spl_autoload_register(function($nombreClase){
+		require_once '../controladores/'.$nombreClase.'.php';
+	});
+
+ 	require_once '../controladores/ProductoController.php';
+
+ 	# Recibimos los datos leídos de php://input
+	$datosRecibidos = file_get_contents("php://input");
+	# No los hemos decodificado, así que lo hacemos de una vez:
+	$getDatos = json_decode($datosRecibidos);
+
+	$metodo = $getDatos->metodo;
+
+ 	$producto = new ProductoController();
+
+ 	// Obtener codigo
+	if($metodo == 1){
+ 		$json_cod = $producto->getProductoCod();
+ 		echo json_encode($json_cod);
+ 	}
+
+ 	// Registrar producto
+ 	if ($metodo == 2) {
+ 		$codigo = $getDatos->codigo;
+ 		$nombre = $getDatos->nombre;
+ 		$descripcion = $getDatos->descripcion;
+ 		$codigo_uni = $getDatos->codigo_uni;
+ 		$fec_venc = $getDatos->fec_venc;
+ 		$codigo_prov = $getDatos->codigo_prov;
+
+ 		$datos=[$codigo, $nombre, $descripcion, $codigo_uni, $fec_venc, $codigo_prov];
+
+ 		$json_registrar = $producto->agregarProducto($datos);
+ 		echo json_encode($json_registrar);
+ 	}
+
+ 	// Modificar producto
+ 	if ($metodo == 3) {
+ 		$codigo = $getDatos->codigo;
+ 		$nombre = $getDatos->nombre;
+ 		$descripcion = $getDatos->descripcion;
+ 		$codigo_uni = $getDatos->codigo_uni;
+ 		$fec_venc = $getDatos->fec_venc;
+ 		$codigo_prov = $getDatos->codigo_prov;
+
+ 		$datos=[$codigo, $nombre, $descripcion, $codigo_uni, $fec_venc, $codigo_prov];
+
+ 		$json_modificar = $producto->modificarProducto($datos);
+ 		echo json_encode($json_modificar);
+ 	}
+
+ 	// Listar productos
+ 	if ($metodo == 4) {
+ 		$json_listar = $producto->listarProductos();
+ 		echo json_encode($json_listar);
+ 	}
+
+ 	// Listar producto por codigo
+ 	if ($metodo == 5) {
+ 		$codigo = $getDatos->codigo;
+ 		$json_list = $producto->getProductoByCod($codigo);
+ 		echo json_encode($json_list);
+ 	}
+
+ 	// Eliminar producto
+ 	if ($metodo == 6) {
+ 		$codigo = $getDatos->codigo;
+ 		$json_eliminar = $producto->eliminarProducto($codigo);
+ 		echo json_encode($json_eliminar);
+ 	}
+
+ 	// Buscar producto
+ 	if ($metodo == 7) {
+ 		$dato = $getDatos->dato;
+ 		$filtro = $getDatos->filtro;
+ 		$json_buscar = $producto->buscarProducto($dato, $filtro);
+ 		echo json_encode($json_buscar);
+ 	}
+
+ ?>
