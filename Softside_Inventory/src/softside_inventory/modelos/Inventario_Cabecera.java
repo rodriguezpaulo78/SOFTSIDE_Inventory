474345ef19765ffc9febdb3cfcd2ab7d3b9c12c9
Paulo Rodriguez <rodriguezpaulo78@gmail.com>
2019-11-20 23:32:35 -0500

    Gestión de Inventario - Modelo Inventario Cabecera v1.0

---
+++ Softside_Inventory/src/softside_inventory/modelos/Inventario_Cabecera.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package softside_inventory.modelos;
+
+/**
+ * Representacion de la entidad Cabecera de Inventario  de la base de datos
+ * 
+ * Controla y gestiona acceso a la base de datos, consultas y peticiones de 
+ * manipulacion de datos para la tabla 'softside' en MySQL. 
+ * Incluye las funciones de insertar y eliminar.
+ * 
+ * @author SOFTSIDE
+ */
+public class Inventario_Cabecera {
+    
+    private String proCod;
+    private String almCod = "0001"; //almCod
+    private String InvCabEstReg;
+}
